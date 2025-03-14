1004f42f4ca7b029a1cce24f55550c0d4d629002
Brenda Nascimento <162237685+Behguedes@users.noreply.github.com>
2025-03-14 19:01:09 -0300

    commit ex015

--- ex015-AluguelCarros.py
+++ ex015-AluguelCarros.py
@@ -1,1 +1,4 @@
-dia = (input('Quantos dias alugado? '))
+dia = int(input('Quantos dias alugados? '))
+km = float(input('Quantos Km rodados? '))
+valor = (60 * dia) + (km * 0.15)
+print('O total a pagar é de R${:.2f}!'.format(valor))
