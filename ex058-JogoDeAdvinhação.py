ade66bc4f07bdd90cfad92c4f3ff723f3bba2165
Brenda Nascimento <162237685+Behguedes@users.noreply.github.com>
2025-04-12 18:04:59 -0300

    commit ex058

---
+++ ex058-JogoDeAdvinhação.py
@@ -0,0 +1,15 @@
+from random import randint
+computador = randint (0, 10)
+print('Sou seu computador... Acabei de pensar em um número de 0 a 10.')
+print('Será que você consegue adivinhar qual foi?')
+cont = 1
+palpite = int(input('Qual é o seu palpite? '))
+while palpite != computador:
+        if palpite > computador:
+            print('Menos...', end=' ')
+        elif palpite < computador:
+           print('Mais...', end=' ')
+        cont += 1
+        palpite = int(input('Tente outra vez: '))
+print('Jogador venceu com {} tentativas. Parabéns!'.format(cont))
+
