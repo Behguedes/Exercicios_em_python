68c8ee827430c07ba87ae04e612ba0e73e5b6323
Brenda Nascimento <162237685+Behguedes@users.noreply.github.com>
2025-04-14 15:39:11 -0300

    commit ex066

--- ex066-VariosNumeros.py
+++ ex066-VariosNumeros.py
@@ -3,6 +3,6 @@ while True:
     num = int(input('Digite um valor (999 para parar): '))
     if num == 999:
         break
-    soma += num
     cont += 1
+    soma += num
 print(f'Foram digitados {cont} números e a soma é {soma}!')
