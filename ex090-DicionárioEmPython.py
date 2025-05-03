a3d25b2f26e78f698d9a3206f8f3743737b26cce
Brenda Nascimento <162237685+Behguedes@users.noreply.github.com>
2025-05-03 00:00:18 -0300

    commit ex090

---
+++ ex090-DicionárioEmPython.py
@@ -0,0 +1,12 @@
+dicionario = {}
+dicionario['Nome'] = str(input('Nome: '))
+dicionario['Média'] = float(input(f'Média de {dicionario['Nome']}: '))
+print('-=' * 20)
+if dicionario['Média'] >= 7:
+    dicionario['Situação'] = 'Aprovado'
+elif 5 <= dicionario['Média'] < 7:
+    dicionario['Situação'] = 'Recuperação'
+else:
+    dicionario['Situação'] = 'Reprovado'
+for n, m in dicionario.items():
+    print(f'{n} é igual a {m}')
