e1e3c253ddfb50dde597b5a7f4de95f2db10680c
Brenda Nascimento <162237685+Behguedes@users.noreply.github.com>
2025-05-03 03:47:33 -0300

    commit ex093

---
+++ ex093-CadastroDeJogadorDeFutebol.py
@@ -0,0 +1,18 @@
+dicionario = dict()
+dicionario['nome'] = str(input('Nome do jogador: '))
+partidas = int(input(f'Quantas partidas {dicionario["nome"]} jogou? '))
+gols = []
+for v in range(partidas):
+    gols.append(int(input(f'Quantos gols na partida {v}? ')))
+dicionario['gols'] = gols
+dicionario['total'] = sum(gols)
+print('-=' * 30)
+print(dicionario)
+print('-=' * 30)
+for k, v in dicionario.items():
+    print(f'O campo {k} tem o valor {v}.')
+print('-=' * 30)
+print(f'O jogador {dicionario['nome']} jogou {partidas} partidas.')
+for i, v in enumerate(gols):
+    print(f'    => Na partida {i}, fez {v} gols.')
+print(f'Foi um total de {dicionario['total']} gols.')
