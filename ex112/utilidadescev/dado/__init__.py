0f62ea4c9a4bba0d2735fea7be16597455fe6c3a
Brenda Nascimento <162237685+Behguedes@users.noreply.github.com>
2025-05-07 22:46:14 -0300

    commit add leiaInt

--- ex112/utilidadescev/dado/__init__.py
+++ ex112/utilidadescev/dado/__init__.py
@@ -7,3 +7,17 @@ def leiaDinheiro(msg):
         else:
             valido = True
             return float(entrada)
+
+def leiaInt(msg):
+    ok = False
+    valor = 0
+    while True:
+        n = str(input(msg))
+        if n.isnumeric():
+            valor = int(n)
+            ok = True
+        else:
+            print('\033[0;31mERRO! Digite um número inteiro válido.\033[m')
+        if ok:
+            break
+    return valor
