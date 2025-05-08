331814d7e6316ab340af4d1063ffeb300fb69b4b
Brenda Nascimento <162237685+Behguedes@users.noreply.github.com>
2025-05-08 20:37:14 -0300

    commit ex115b

--- ex115/sistema.py
+++ ex115/sistema.py
@@ -1,10 +1,17 @@
 from ex115.lib.interface import *
+from ex115.lib.arquivo import *
 from time import sleep
+
+arq = 'cursoemvideo.txt'
+
+if not arquivoExiste(arq):
+    criarArquivo(arq)
 
 while True:
     resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova Pessoas', 'Sair do Sistema'])
     if resposta == 1:
-        cabeçalho('Opção 1')
+        # Opção de listar o conteúdo de um arquivo!
+        lerArquivo(arq)
     elif resposta == 2:
         cabeçalho('Opção 2')
     elif resposta == 3:
